4e673d7c9622fe85ff408359d95801492531a919
michallow <michallo_w@o2.pl>
2024-01-15 19:17:34 +0100

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,4 @@
+const categoriesUl = document.getElementById('categories');
+const categoryItems = categoriesUl.querySelectorAll('li.item');
+const numberOfCategories = categoryItems.length;
+console.log('Liczba kategorii:', numberOfCategories);
