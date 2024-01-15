9f0e58a8e4f37d344f7485df46f6459143f4562b
michallow <michallo_w@o2.pl>
2024-01-15 21:08:20 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,12 @@
-const categoriesUl = document.getElementById('categories');
-const categoryItems = categoriesUl.querySelectorAll('li.item');
+const categoriesUl = document.getElementById("categories");
+const categoryItems = categoriesUl.querySelectorAll("li.item");
 const numberOfCategories = categoryItems.length;
-console.log('Liczba kategorii:', numberOfCategories);
+console.log("Liczba kategorii:", numberOfCategories);
+
+for (const i = 0; i < categoryItems.length; i++) {
+  const header = items[i].getElementsByTagName("h2")[0].textContent;
+  const categoryItems = items[i].getElementsByTagName("li")[0].length;
+
+  console.log("Kategoria:", header);
+  console.log("Liczba elementów", categoryItems);
+}
