7f7bd3c07739a2713ba700652bc1803fedd6c361
michallow <michallo_w@o2.pl>
2024-01-15 21:10:52 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,7 @@
 const categoriesUl = document.getElementById("categories");
 const categoryItems = categoriesUl.querySelectorAll("li.item");
 const numberOfCategories = categoryItems.length;
+
 console.log("Liczba kategorii:", numberOfCategories);
 
 for (let i = 0; i < categoryItems.length; i++) {
