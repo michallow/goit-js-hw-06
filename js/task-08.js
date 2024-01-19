79672dc857c05ee646253ce75ad462544152ccda
michallow <michallo_w@o2.pl>
2024-01-19 11:17:40 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -3,14 +3,23 @@ const loginForm = document.querySelector(".login-form");
 loginForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
-    event.preventDefault();
-    const form = event.target;
-    const email = form.elements.email.value;
-    const password = form.elements.password.value;
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
 
-    if (email === "" || password === "") {
-        alert("Please fill in all the fields!");
-      }
-    
-      form.reset()
+  if (email === "" || password === "") {
+    alert("Please fill in all the fields!");
+    return;
+  }
+
+  const formData = {};
+  Array.from(form.elements).forEach((element) => {
+    if (element.type !== "submit" && element.type !== "reset") {
+      formData[element.name] = element.value;
+    }
+  });
+
+  console.log("Form Data:", formData);
+  form.reset();
 }
