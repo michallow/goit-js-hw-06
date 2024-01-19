4efabafdaff6c0ec3c63d7b2d5d4261de4426024
michallow <michallo_w@o2.pl>
2024-01-19 12:33:44 +0100

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const colorSpan = document.querySelector(".color");
+const colorButton = document.querySelector(".change-color");
+
+colorButton.addEventListener("click", function () {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
